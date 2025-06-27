e16216380996312b30aea68291e2717ccdcc6c6d
kamalcherala <22071a1012@vnrvjiet.in>
2025-06-27 13:03:00 +0530

    server.js

--- backend/express-api/server.js
+++ backend/express-api/server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://rewards-platform.vercel.app/",
     credentials: true,
   })
 );
