df7d4bba8be0ded0036faf258aa91cb9834cd133
kamalcherala <22071a1012@vnrvjiet.in>
2025-06-27 12:58:05 +0530

    server.js

--- backend/express-api/server.js
+++ backend/express-api/server.js
@@ -45,3 +45,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+// API root route
+app.get("/", (req, res) => {
+  res.send("Rewards Platform API is live!");
+});
